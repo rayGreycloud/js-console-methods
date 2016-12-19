033b62c2df5beafee7c42a71ece478a3b5081697
rayGreycloud <rayGreycloud@gmail.com>
2016-12-19 07:46:54 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// Console Methods
+
+// Regular
+
+// Interpolated
+
+// Styled
+
+// Warning!
+
+// Error :|
+
+// Info
+
+// Testing
+
+// Clearing
+
+// Viewing DOM Elements
+
+// Grouping together
+
+// Counting
+
+// Timing
